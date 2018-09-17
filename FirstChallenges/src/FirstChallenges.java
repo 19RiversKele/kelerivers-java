370812ae97a4740b6c249f0f10d13963250d2883
19RiversKele <keletamasrivers@gmail.com>
2018-09-17 09:21:34 -0700

    First Challenges

---
+++ FirstChallenges/src/FirstChallenges.java
@@ -0,0 +1,78 @@
+public class FirstChallenges {
+    public static void main(String[] args) {
+        lineExponent10000();
+        fourBlot();
+    }
+    public static void lineExponent10() {
+        System.out.println("line");
+        System.out.println("line");
+        System.out.println("line");
+        System.out.println("line");
+        System.out.println("line");
+        System.out.println("line");
+        System.out.println("line");
+        System.out.println("line");
+        System.out.println("line");
+        System.out.println("line");
+    }
+    public static void lineExponent100() {
+        lineExponent10();
+        lineExponent10();
+        lineExponent10();
+        lineExponent10();
+        lineExponent10();
+        lineExponent10();
+        lineExponent10();
+        lineExponent10();
+        lineExponent10();
+        lineExponent10();
+    }
+    public static void lineExponent1000() {
+        lineExponent100();
+        lineExponent100();
+        lineExponent100();
+        lineExponent100();
+        lineExponent100();
+        lineExponent100();
+        lineExponent100();
+        lineExponent100();
+        lineExponent100();
+        lineExponent100();
+    }
+    public static void lineExponent10000() {
+        lineExponent1000();
+        lineExponent1000();
+        lineExponent1000();
+        lineExponent1000();
+        lineExponent1000();
+        lineExponent1000();
+        lineExponent1000();
+        lineExponent1000();
+        lineExponent1000();
+        lineExponent1000();
+    }
+    public static void blot() {
+        System.out.println("*");
+    }
+    public static void twoBlot() {
+        blot();
+        blot();
+        blot();
+        blot();
+        blot();
+    }
+    public static void threeBlot() {
+        twoBlot();
+        twoBlot();
+        twoBlot();
+        twoBlot();
+        twoBlot();
+    }
+    public static void fourBlot() {
+        threeBlot();
+        threeBlot();
+        threeBlot();
+        threeBlot();
+        threeBlot();
+    }
+}
