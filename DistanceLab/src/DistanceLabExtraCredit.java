55252a27627e8109651fa0109601a479b9576a32
19RiversKele <keletamasrivers@gmail.com>
2018-10-22 09:21:42 -0700

    Distance Lab Default + Beginning of EC

---
+++ DistanceLab/src/DistanceLabExtraCredit.java
@@ -0,0 +1,11 @@
+public class DistanceLabExtraCredit {
+    public static void main(String[] args) {
+        String test = "coordinates";
+        System.out.println(test.indexOf('d'));
+
+        String GridZoneDesignationLetters = "CDEFGHJKLMNPQRSTUVWX";
+
+
+
+    }
+}
