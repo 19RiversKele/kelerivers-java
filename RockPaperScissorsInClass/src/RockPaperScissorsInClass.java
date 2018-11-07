4a346854bc980b2724dc42b98885d2e3a14b1127
19RiversKele <keletamasrivers@gmail.com>
2018-11-07 09:27:37 -0800

    update 3 - Added comments, heading, and invalid input no longer counts as a round

--- RockPaperScissorsInClass/src/RockPaperScissorsInClass.java
+++ RockPaperScissorsInClass/src/RockPaperScissorsInClass.java
@@ -1,54 +1,65 @@
+/*
+***************************************
+* Kele Rivers                         *
+* AP CS A                             *
+* Period 1                            *
+* November 7, 2018                    *
+* Rock Paper Scissors lab             *
+***************************************
+*/
+
+
 import java.util.Scanner;
 
 public class RockPaperScissorsInClass {
-
+//These variables are for the scoreboard, not final so they can be updated
     public static int WINS = 0;
     public static int TIES = 0;
     public static int LOSSES = 0;
 
 
     public static void main(String[] args) {
+     //Sets up username
         Scanner userName = new Scanner(System.in);
         System.out.print("Enter a username: ");
         String playerID = userName.next();
         System.out.println("Welcome " + playerID);
-
+     //Determines if you want to play a game in the first place
         System.out.println("Would you like to play a game? " + playerID + " Yes or no?");
         Scanner playGame = new Scanner(System.in);
         String playAGame = playGame.next();
         playAGame = playAGame.toLowerCase();
 
         while (playAGame.equals("yes")) {
-
+         //Resets the scoreboard at the beginning of a game
             WINS = 0;
             TIES = 0;
             LOSSES = 0;
-
+         //Sets number of rounds to determine a game
             System.out.println("How many rounds would you like to play? 3 or 5?");
             Scanner numberOfRounds = new Scanner(System.in);
             int numOfRounds = numberOfRounds.nextInt();
-
-
+         //Runs each round for the game
             int roundNumber = 0;
             while (roundNumber < numOfRounds) {
-
+             //Tells user what they picked
                 String userWeapon = userChoice();
                 System.out.println("User picked " + userWeapon);
-
+             //Tells user what the computer picked
                 String computerChoice = getComputerChoice();
                 System.out.println("Computer picked " + computerChoice);
-
+             //Compares choices and tells user who won
                 String roundResult = (getResults(userWeapon, computerChoice));
                 System.out.println(roundResult);
-
-                if (roundResult.equals("TIED GAME")) {
+             //Round does not count if tie
+                if (roundResult.equals("TIED GAME") || roundResult.equals("---ERROR DETERMINING WINNER---")) {
                     roundNumber--;
                 }
 
-
+             //New round
                 roundNumber++;
             }
-
+           //Scoreboard
             System.out.println("********************************");
             if (WINS > LOSSES) {
                 System.out.println("USER HAS WON THE GAME!");
@@ -59,7 +70,7 @@ public class RockPaperScissorsInClass {
             System.out.println("Rounds won by computer: " + LOSSES);
             System.out.println("Rounds tied: " + TIES);
             System.out.println("********************************\n");
-
+         //Asks user if they would like to play again, if yes sets up a new game
             System.out.println("Would you like to play again? Yes or no?");
             playAGame = playGame.next();
             playAGame = playAGame.toLowerCase();
@@ -69,7 +80,7 @@ public class RockPaperScissorsInClass {
         System.out.println("Thank you for playing!");
 
     }
-
+   //Prepares user choice
     public static String userChoice() {
         System.out.println("What do you choose? Rock, paper, or scissors?   :   ");
         Scanner cInput = new Scanner(System.in);
@@ -77,7 +88,7 @@ public class RockPaperScissorsInClass {
         userWeapon = userWeapon.toLowerCase();
         return(userWeapon);
     }
-
+   //Generates and prepares computerChoice
     public static String getComputerChoice() {
         double computerChoiceGenerator = Math.floor(Math.random() * 3);
         if (computerChoiceGenerator == 0.0) {
@@ -92,7 +103,7 @@ public class RockPaperScissorsInClass {
         }
     }
 
-
+   //Compares userChoice and computerChoice to determine the winner
     public static String getResults(String userChoice, String computerChoice) {
         if (computerChoice.equals(userChoice)) {
             TIES++;
