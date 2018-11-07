a5fd6e772eb73e73d9f72bcda92a1483c1529c11
19RiversKele <keletamasrivers@gmail.com>
2018-11-07 10:11:45 -0800

    First part of Distance Extra Credit

---
+++ DistanceLabExtraCredit/src/DistanceLabExtraCredit.java
@@ -0,0 +1,43 @@
+import java.util.Scanner;
+
+
+public class DistanceLabExtraCredit {
+
+    public static String COORDINATE1;
+    public static String COORDINATE2;
+    public static int PRECISION;
+
+    public static void main(String[] args) {
+        Scanner firstCoordinate = new Scanner(System.in);
+        Scanner secondCoordinate = new Scanner(System.in);
+        Scanner precisionLevel = new Scanner(System.in);
+
+        System.out.print("What precision level are you inputing? 1 square meter, 10, 100, 1000, or 10,000?: ");
+        PRECISION = precisionLevel.nextInt();
+
+        System.out.print("\nImport first coordinate: ");
+        COORDINATE1 = firstCoordinate.next();
+
+        System.out.print("\nImport second coordinate: ");
+        COORDINATE2 = secondCoordinate.next();
+
+        singleDigitToDoubleDigitFix(COORDINATE1, COORDINATE2);
+
+        System.out.println(COORDINATE1);
+        System.out.println(COORDINATE2);
+
+
+
+    }
+
+    public static void singleDigitToDoubleDigitFix(String coordinate1, String coordinate2) {
+        char coordOneCheck = coordinate1.charAt(5);
+        char coordTwoCheck = coordinate2.charAt(5);
+        if (Character.isDigit(coordOneCheck)) {
+            COORDINATE1 = "0" + COORDINATE1;
+        }
+        if (Character.isDigit(coordTwoCheck)) {
+            COORDINATE2 = "0" + COORDINATE2;
+        }
+    }
+}
